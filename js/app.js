5543f5ac381e2f6295c40213310e1e21df8e3ca7
SergioSaulAM <ssaulam34@gmail.com>
2023-03-26 22:46:02 -0600

    Agregando Materialize a Sitio Wen

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const modal = document.querySelector("#modal");
+const abrirModal = document.querySelector("#abrir-modal");
+const cerrarModal = document.querySelector("#cerrar-modal");
+const enviarModal = document.querySelector("#enviar-modal");
+
+const addPost = document.querySelector("#add-post");
+const addPostModal = document.querySelector("#add-post-modal");
+const closePostModal = document.querySelector("#cerrar-modal-post");
+const enviarPostModal = document.querySelector("#enviar-modal-post");
+
+
+const input = document.getElementsByTagName("input");
+
+const limpiar = (target) => {
+    if (target.value !== "") {
+        target.value = "";
+    }
+}
+
+abrirModal.addEventListener("click", () => {
+    modal.showModal();
+})
+
+cerrarModal.addEventListener("click", () => {
+    modal.close();
+})
+
+enviarModal.addEventListener("click", () => {
+    modal.close();
+})
+
+addPost.addEventListener("click", () => {
+    addPostModal.showModal();
+})
+
+closePostModal.addEventListener("click", () => {
+    addPostModal.close()
+})
+
+enviarPostModal.addEventListener("click", () => {
+    addPostModal.close()
+})
