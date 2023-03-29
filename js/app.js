1dd65fba80994268e99daa4c23a3a5fd26cbea1e
SergioSaulAM <ssaulam34@gmail.com>
2023-03-29 07:25:55 -0600

    Agregando modificaciones de botones de modificación y eliminación de post

--- js/app.js
+++ js/app.js
@@ -12,11 +12,23 @@ const input = document.getElementsByTagName("input");
 
 const addPostRespo = document.querySelector("#add-post-respo");
 
+const modifyPost = document.getElementsByClassName("modify-post");
+const deletePost = document.getElementsByClassName("delete-post");
+
+
+/* const modifyPost = document.querySelectorAll("modify-post");
+const deletePost = document.querySelectorAll("delete-post"); */
+
+const post = document.querySelector(".post");
+
 const limpiar = (target) => {
     if (target.value !== "") {
         target.value = "";
     }
 }
+
+console.log(modifyPost);
+console.log(deletePost);
 
 abrirModal.addEventListener("click", () => {
     modal.showModal();
@@ -44,4 +56,28 @@ enviarPostModal.addEventListener("click", () => {
 
 addPostRespo.addEventListener("click", () => {
     addPostModal.showModal()
+})
+
+modifyPost["0"].addEventListener("click", () => {
+    addPostModal.showModal()
+})
+
+deletePost["0"].addEventListener("click", () => {
+    post.classList.add("borrar");
+})
+
+modifyPost["1"].addEventListener("click", () => {
+    addPostModal.showModal()
+})
+
+deletePost["1"].addEventListener("click", () => {
+    post.classList.add("borrar");
+})
+
+modifyPost["2"].addEventListener("click", () => {
+    addPostModal.showModal()
+})
+
+deletePost["2"].addEventListener("click", () => {
+    post.classList.add("borrar");
 })
